0c3edf7f0acd270233fec9198a47fd0ce3b48db1
Byron <byron@atebol.com>
2017-08-14 16:56:46 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,1 @@
+import Core from './app/components/Core.jsx'
---
+++ app/components/Core.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Postal from 'postal';
+import '../less/styles.less';
+
+
+class Core extends React.Component {
+
+
+
+
+}
+export default Core;
